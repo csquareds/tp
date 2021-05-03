14943cad96f52ca9bc8771892a5ffd7ae6aeee21
Catherine Cai <83183182+csquareds@users.noreply.github.com>
2021-05-02 22:23:19 -0400

    Add files via upload

---
+++ hauntinfo.py
@@ -0,0 +1,74 @@
+# HAUNT INFO
+
+# CHECKMATE HAUNT
+heroText1 = ['You’ve finally figured out what the cramped handwriting in that old journal stated:',
+    '“I, Ebenezer Slocum, have found the means to force Death itself to appear before me. I,',
+    'have prepared to challenge Death, and I will defeat it! Through study, I have enhanced my mind to its',
+    'keenest. Oh, Death will not be proud this night!” Looking around, you notice a figure slumped at a',
+    'chessboard. As you touch the figure, it crumbles to dust. Looks as if Ebenezer wasn’t quite as prepared',
+    'as he thought. Across the table, you see a dark, shadowy figure appear. The figure beckons toward',
+    'one of you and points to the table. An ebony and ivory chess set sits between the two of you.',
+    'You hope you’re better than Ebenezer was.']
+heroText2 = ['What You Know About the Bad Guys: Death has challenged you to a chess game.',
+     'If there is no one in the room to play against Death at the',
+    'beginning of Death’s turn, you forfeit the chess game and lose.']
+heroText3 = ['You Win When ...',
+    '... you checkmate Death by rolling a higher result',
+    'than it does on a Knowledge roll. Once during each',
+    'of Death’s turns, one of the heroes can attempt this',
+    'roll while in the same room.']
+heroText4 = ['How to Beat Death',
+    'Some items in the house can help you get a higher result on a Knowledge roll than Death can:',
+    '• Explorers can pick up Holy Seal tokens. If you pick up a Holy Seal, you can attempt a',
+    'Sanity roll of 4+ to break it. You can only break one Holy Seal during your turn. Each time an',
+    'explorer breaks a Holy Seal, Death rolls one fewer dice on its subsequent Knowledge rolls. If',
+    'there are only three or four players, it rolls two fewer dice instead.',
+    '• The Book contains chess strategies. The explorer who has it can add one die (maximum',
+    'eight dice) to Knowledge rolls attempted while playing chess against Death.']
+heroText5 = ['Special Attack Rules',
+    'Death can’t attack or be affected in any way other than by being beaten at chess.',
+    'Death does not slow your movement.']
+heroText6 = ['If You Win ...',
+    '“Checkmate.” Death stares fixedly at his King, then starts',
+    'crumbling into dust. Death smiles, and you feel your hair turn white.',
+    '“Until next time,” Death responds ....']
+traitorText1 = ['You know what those fools are reading in Uncle Ebenezer’s journal: “I, Ebenezer',
+    'Slocum, have found the means to force Death itself to appear before me. I have prepared to challenge',
+    'Death, and I will defeat it! I have studied and enhanced my mind to its keenest. Oh, Death will not',
+    'be proud this night!” Well, that old geezer’s bones are still sitting by the chess set. You doubt these',
+    'fools can do any better. Beat Death?! Indeed! Just in case, you’ll do what you can to make sure none',
+    'of them win this game. After all, you can’t stand the thought of anyone being smarter than you,',
+    'and there’s no way you could beat Death at chess!']
+traitorText2 = ['Right Now',
+    'Your character is still in the game but has turned traitor. Put the Death token (light green)',
+    'in a room with a hero of your choice. Put the five Holy Seal tokens (pentagonal) in the',
+    'following rooms, either now or when they are discovered: the Vault, the Crypt,',
+    'the Research Laboratory, the Operating Laboratory, and the Game Room. Tell the heroes',
+    'that there are five Holy Seals, but don’t tell them which undiscovered rooms have them.']
+traitorText3 = ['What You Know About the Heroes',
+    'One of them will try to beat Death in a game of chess. They’ll use the',
+    'Holy Seals to help. If they beat Death even once, you will lose.',
+    'You Win When ...',
+    '... all of the heroes are dead. You also win if there is no explorer',
+    'in the room with Death at the beginning of the monster’s turn.',
+    '(If no one is there, the heroes forfeit their game of chess.)']
+traitorText4 = ['Death Must Do This On Its Turn',
+    'During its turn, Death plays against the explorer in the room with the highest Knowledge.',
+    'Both attempt Knowledge rolls. Death has a Knowledge of 8, and it cheats. After its first roll,',
+    'it rerolls any blank dice. (For example, if Death rolls 8 dice, and 2 of them have no successes,',
+    'those two dice are “blank dice.” Those two blank dice are then rerolled, but only once.)',
+    'Death and its opponent then compare results. If the result is a tie, nothing happens.',
+    'If Death rolls a higher result, it captures a piece:',
+    '• If Death wins by 1 or 2, it takes a pawn and each hero loses 1 Sanity.',
+    '• If Death wins by 3 or 4, it takes a significant piece and each hero loses 1 Might.',
+    '• If Death wins by 5 or more, it grimly intones the word “check.” Each hero loses 1 Sanity and 1 Might.']
+traitorText5 = ['Special Attack Rules',
+    'Death can’t attack or be affected in any way other than by being beaten at chess.',
+    'You can’t enter the room with Death or affect a hero there in any way, such as by',
+    'using the Bell, the Revolver, or the Dynamite. (Your master doesn’t like distractions',
+    'from his game.) You can’t pick up Holy Seals, but you can steal them from the heroes.']
+traitorText6 = ['If You Win ...',
+    'Ha! The fools. You knew they couldn’t beat Death at his',
+    'own game. So what if you helped Death out a bit. The important thing is they’re dead',
+    'and gone, and you’re still here.',
+    'Checkmate.']
